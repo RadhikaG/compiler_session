d527e81cf7a1302ac4a04d670bd6600767b1d247
RadhikaG <radhikaghosal@gmail.com>
2017-12-03 12:35:25 +0530

    uncomment main

--- examples/woo.c
+++ examples/woo.c
@@ -1,9 +1,9 @@
 #include "woo.h"
 
-/*int main(void) {*/
-    /*woo(1, 2);*/
-    /*return 0;*/
-/*}*/
+int main(void) {
+    woo(1, 2);
+    return 0;
+}
 
 #ifdef WOO_FUNC
 int woo(int a, int b) {
